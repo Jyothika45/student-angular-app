b5b28518e7d161c399682d61363cfd36e239ef53
Jyothika45 <jyothikakt55@gmail.com>
2026-07-21 12:20:48 +0530

    updated

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('campus360', []);
+
+app.controller("dashboardController", function($scope){
+
+    console.log("Dashboard Controller is loaded");
+
+    $scope.collegename = "FISAT";
+    $scope.totstud = "60";
+    $scope.totfac = "10";
+    $scope.name = "Jyothika";
+    $scope.department = "Computer Science";
+    $scope.email = "jyothika@example.com";
+    $scope.phone = "1234567890";
+    console.log("dashboardController is loaded");
+
+$scope.students = [
+    "Alice", "Appu", "Ammu", "Kuttu"
+];
+
+
+    $scope.addStudent = function() {
+        $scope.totsud++;
+
+
+    };
+
+
+});
