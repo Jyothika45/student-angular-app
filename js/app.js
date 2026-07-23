1630ccc46b4840523a1e9a4adbfc45beea426ca2
Jyothika45 <jyothikakt55@gmail.com>
2026-07-23 12:13:14 +0530

    done

--- js/app.js
+++ js/app.js
@@ -1,48 +1,60 @@
 var app = angular.module('campus360', []);
 
-app.controller("dashboardController", function ($scope) {
+app.controller("dashboardcontroller", function ($scope) {
 
-    console.log("Dashboard Controller is loaded");
 
     $scope.collegename = "FISAT";
-    $scope.totstud = 60;
-    $scope.totfac = 10;
-    $scope.name = "Jyothika";
-    $scope.department = "Computer Science";
-    $scope.email = "jyothika@example.com";
-    $scope.phone = "1234567890";
-    $scope.showstudent = false;
+    $scope.totalstudents = 60;
+    $scope.totalfaculty = 50;
+
+    console.log("dashboard controller is loaded");
 
     $scope.students = [
-        "Alice","Appu","Ammu","Kuttu"
+        "achu", "appu", "ammu", "jyothika"
     ];
 
+  
+        $scope.showstudents = false;
+        $scope.showDashboard = true;
+
+        $scope.fees = 25000;
+        $scope.today = new Date();
+
+        $scope.departments=[
+            "CSE","MCA","MBA","MCOM","ECE","IMCA","BCA"
+        ];
+
+        $scope.selectedDept = "MCA";
 
 
-    $scope.fees = 1000;
-    $scope.today = new Date();
+        $scope.isDisabled = true;
+        $scope.allowAdmission = function(){
 
-    $scope.departments = ["CSE", "ECE", "EEE", "MECH", "CIVIL","MCA"];
-    $scope.selectedDepartments = ["MCA"];
-
-    $scope.isDisabled=true;
-    $scope.allowAdmission=function(){
-        $scope.isDisabled=false;
-    };
+            $scope.isDisabled = false;
+        };
 
 
-$scope.isReadOnly=true;
-$scope.toggleReadOnly=function(){
-    $scope.isReadOnly=!$scope.isReadOnly;
-};
+        $scope.isReadOnly = true;
+        $scope.toogleReadOnly = function (){
+            
+            $scope.isReadOnly = !$scope.isReadOnly;
+        };
 
 
+        $scope.student={};
+        $scope.registerstudent = function () {
 
+            if ($scope.studentForm.$valid) {
+                alert("Student Registered Successfully");
+            }
+            else {
+                alert("Please Fill All Required Fields Correctly");
+            }
 
+        };
 
-
-    $scope.addStudent = function () {
-        $scope.totstud++;
+    $scope.addstudents = function () {
+        $scope.totalstudents++;
     };
 
 });
